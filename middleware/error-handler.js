dee27de281fc2a1754bdde45cb28e75972fc8d5c
Uroš Todorović <todorovic.urosh@gmail.com>
2023-03-20 11:38:04 +0100

    added password hashing as a pre save hook in User model

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+	console.log(err);
 	const defaultError = {
 		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
 		message: err.message || 'Something went wrong, please try again later.',
@@ -11,6 +12,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
 		defaultError.message = Object.values(err.errors)
 			.map((error) => error.message)
 			.join(', ');
+	}
+
+	if (err.code && err.code === 11000) {
+		defaultError.statusCode = StatusCodes.BAD_REQUEST;
+		defaultError.message = `${Object.keys(err.keyValue)} has to be unique`;
 	}
 	res.status(defaultError.statusCode).json({ message: defaultError.message });
 };
