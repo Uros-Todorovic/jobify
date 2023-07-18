fdfe1f8113fce04296dd8dc75ec76d3557f41f79
Uroš Todorović <todorovic.urosh@gmail.com>
2023-07-18 22:08:43 +0200

    frontend adding job functionality added

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -9,6 +9,11 @@ import {
 	UPDATE_USER_BEGIN,
 	UPDATE_USER_SUCCESS,
 	UPDATE_USER_ERROR,
+	HANDLE_CHANGE,
+	CLEAR_VALUES,
+	CREATE_JOB_BEGIN,
+	CREATE_JOB_SUCCESS,
+	CREATE_JOB_ERROR,
 } from './actions';
 import { initialState } from './appContext';
 
@@ -98,6 +103,56 @@ const reducer = (state, action) => {
 	}
 
 	if (action.type === UPDATE_USER_ERROR) {
+		return {
+			...state,
+			showAlert: true,
+			alertType: 'danger',
+			alertText: action.payload.message,
+			isLoading: false,
+		};
+	}
+
+	if (action.type === HANDLE_CHANGE) {
+		return {
+			...state,
+			[action.payload.name]: action.payload.value,
+		};
+	}
+
+	if (action.type === CLEAR_VALUES) {
+		const initialState = {
+			isEditing: false,
+			editJobId: '',
+			position: '',
+			company: '',
+			jobLocation: state.userLocation,
+			jobType: 'full-time',
+			status: 'pending',
+		};
+		return {
+			...state,
+			...initialState,
+		};
+	}
+
+	if (action.type === CREATE_JOB_BEGIN) {
+		return {
+			...state,
+			isLoading: true,
+		};
+	}
+
+	if (action.type === CREATE_JOB_SUCCESS) {
+		return {
+			...state,
+			showAlert: true,
+			alertType: 'success',
+			alertText: 'New Job Created!',
+			isLoading: false,
+		};
+	}
+
+	if (action.type === CREATE_JOB_ERROR) {
 		return {
 			...state,
 			showAlert: true,
